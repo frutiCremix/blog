02076d52c71a9fccf6806f0126716706dc8028d5
Sebastian Oliveto <sebastian.oliveto@hotmail.com>
2023-12-07 17:36:50 -0300

    fix .env

--- firebase.js
+++ firebase.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const {initializeApp, applicationDefault} = require("firebase-admin/app");
 const {getFirestore}=require('firebase-admin/firestore')
+const admin = require('firebase-admin');
 const {initializeApp: initializeAppClient}= require('firebase/app')
 const { getAuth, signInWithEmailAndPassword,signOut }=require("firebase/auth");
 const {
@@ -13,9 +14,22 @@ getStorage,
   uploadBytes,ref,
   uploadBytesResumable
 } =require("firebase/storage");
+const serviceAccount={
+  "type": process.env.TYPE,
+  "project_id": process.env.PROJECT_ID,
+  "private_key_id":process.env.PRIVATE_KEY_ID,
+  "private_key":process.env.PRIVATE_KEY,
+  "client_email": process.env.CLIENT_EMAIL,
+  "client_id": process.env.CLIENT_ID,
+  "auth_uri": process.env.AUTH_URI,
+  "token_uri": process.env.TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
+  "universe_domain": process.env.UNIVERSE_DOMAIN
+}
 
 const app=initializeApp({
-    credential:applicationDefault()
+    credential:admin.credential.cert(serviceAccount),
 })
 process.env.GOOGLE_APPLICATION_CREDENTIALS;
 //config para la api de cliente
